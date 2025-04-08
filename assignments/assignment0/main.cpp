3b32e28ab1b2a9c0282d442b6fe5f978947334b1
atticus.clark <atticus.clark@mymail.champlain.edu>
2025-04-08 09:10:45 -0400

    moved the stuff that isn't the main function out of main.cpp

--- assignments/assignment0/main.cpp
+++ assignments/assignment0/main.cpp
@@ -1,34 +1,4 @@
-#include <stdio.h>
-#include <math.h>
-
-#include <ew/external/glad.h>
-
-#include <GLFW/glfw3.h>
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-
-#include <ew/shader.h>
-#include <ew/model.h>
-#include <ew/camera.h>
-#include <ew/transform.h>
-#include <ew/cameraController.h>
-#include <ew/texture.h>
-#include <ew/procGen.h>
-
-void framebufferSizeCallback(GLFWwindow* window, int width, int height);
-GLFWwindow* initWindow(const char* title, int width, int height);
-void drawUI();
-void resetCamera(ew::Camera* camera, ew::CameraController* controller);
-
-//Global state
-int screenWidth = 1080;
-int screenHeight = 720;
-float prevFrameTime;
-float deltaTime;
-
-ew::Camera camera;
-ew::CameraController cameraController;
+#include "notmain.h"
 
 int main()
 {
@@ -62,11 +32,11 @@ int main()
 		deltaTime = time - prevFrameTime;
 		prevFrameTime = time;
 
+		cameraController.move(window, &camera, deltaTime);
+
 		//RENDER
 		glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		cameraController.move(window, &camera, deltaTime);
 
 		// shader calls
 		waterShader.use();
@@ -79,69 +49,4 @@ int main()
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
-}
-
-void resetCamera(ew::Camera* camera, ew::CameraController* controller)
-{
-	camera->position = glm::vec3(0, 0, 5.0f);
-	camera->target = glm::vec3(0);
-	controller->yaw = controller->pitch = 0;
-}
-
-void drawUI() {
-	ImGui_ImplGlfw_NewFrame();
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui::NewFrame();
-
-	ImGui::Begin("Settings");
-	if (ImGui::Button("Reset Camera"))
-	{
-		resetCamera(&camera, &cameraController);
-	}
-	ImGui::End();
-
-	ImGui::Render();
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-}
-
-void framebufferSizeCallback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-	screenWidth = width;
-	screenHeight = height;
-}
-
-/// <summary>
-/// Initializes GLFW, GLAD, and IMGUI
-/// </summary>
-/// <param name="title">Window title</param>
-/// <param name="width">Window width</param>
-/// <param name="height">Window height</param>
-/// <returns>Returns window handle on success or null on fail</returns>
-GLFWwindow* initWindow(const char* title, int width, int height) {
-	printf("Initializing...");
-	if (!glfwInit()) {
-		printf("GLFW failed to init!");
-		return nullptr;
-	}
-
-	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
-	if (window == NULL) {
-		printf("GLFW failed to create window");
-		return nullptr;
-	}
-	glfwMakeContextCurrent(window);
-
-	if (!gladLoadGL(glfwGetProcAddress)) {
-		printf("GLAD Failed to load GL headers");
-		return nullptr;
-	}
-
-	//Initialize ImGUI
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init();
-
-	return window;
 }
