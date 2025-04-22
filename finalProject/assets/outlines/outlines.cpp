982ef8526099614b0a1ed2562d3cf3d639882a65
atticus-clark <atticus.clark@mymail.champlain.edu>
2025-04-21 20:56:59 -0400

    xray hacks enabled
    outlines can now be toggled to be visible through non-outlined objects

--- finalProject/assets/outlines/outlines.cpp
+++ finalProject/assets/outlines/outlines.cpp
@@ -37,11 +37,16 @@ void OutlinedObjs::Render(const glm::mat4& viewProj) {
 	// ------------------------------------------------------------------------------------------- //
 	// 1st render pass: Draw objects as normal while writing to the stencil buffer.
 
-	// setup
+	// setup //
+
+	// these slightly change how the stencil buffer is written to
+	// refer to xray.md to learn about what exactly this does
+	xray ? glStencilOp(GL_KEEP, GL_REPLACE, GL_REPLACE) : glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+
 	glStencilFunc(GL_ALWAYS, 1, 0xFF); // makes all fragments pass the stencil test
 	glStencilMask(0xFF); // enable writing to stencil buffer
 
-	// draw objects
+	// draw objects //
 	for(int i = 0; i < NUM_OUTLINED_OBJS; i++) {
 		shader.setMat4("_Model", transforms[i].modelMatrix());
 		shader.setVec4("_Color", colors[i]);
@@ -55,14 +60,15 @@ void OutlinedObjs::Render(const glm::mat4& viewProj) {
 	 * drawn, thus only drawing the size differences between the objects in the 1st and 2nd
 	 * pass, which looks like an outline. */
 
-	// setup 
-	glStencilFunc(GL_NOTEQUAL, 1, 0xFF); // don't draw any fragments that were drawn before
-										 // aka, only draw the scaled-up parts of the objects
+	// setup //
+
+	// don't draw any fragments that were drawn before; aka, only draw the scaled-up parts of the objects
+	glStencilFunc(GL_NOTEQUAL, 1, 0xFF); 
 	glStencilMask(0x00); // disable writing to stencil buffer
 
-	// disabling depth testing makes outline always visible even if object is obscured
-	// if object is obscured, it can't block the scaled-up draw so outline becomes a solid block
-	xray ? glDisable(GL_DEPTH_TEST) : false;
+	// this is what actually determines if outlines are visible through walls or not
+	// refer to xray.md to learn why this only works with the first glStencilOp setup
+	xray ? glDisable(GL_DEPTH_TEST);
 
 	outlineShader.use();
 	outlineShader.setVec4("_Color", outlineColor);
@@ -80,5 +86,5 @@ void OutlinedObjs::Render(const glm::mat4& viewProj) {
 	// cleanup
 	glStencilMask(0xFF);
 	glStencilFunc(GL_ALWAYS, 0, 0xFF);
-	glEnable(GL_DEPTH_TEST); // make sure to reenable depth testing for future frames!
+	xray ? glEnable(GL_DEPTH_TEST); // if depth testing was disabled, make sure to reenable it
 }
